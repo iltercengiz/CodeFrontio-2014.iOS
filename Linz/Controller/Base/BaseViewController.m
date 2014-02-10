488382c2cbb10fb73e11d93c9ed01305d897eccd
Ilter Cengiz <me@iltercengiz.info>
2014-02-10 15:29:13 +0200

    Added BaseViewController
    BaseViewController will implement UISplitViewControllerDelegate and all detail view's will be subclassed from it.

---
+++ Linz/Controller/Base/BaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  BaseViewController.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 10/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
