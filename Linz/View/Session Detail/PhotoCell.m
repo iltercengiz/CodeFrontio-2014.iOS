10102081cacf54b26061de94391045c3a9e205f2
Ilter Cengiz <me@iltercengiz.info>
2014-02-12 00:37:49 +0200

    PhotoCell class added for presenting photos

---
+++ Linz/View/Session Detail/PhotoCell.m
@@ -0,0 +1,31 @@
+//
+//  PhotoCell.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 12/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
