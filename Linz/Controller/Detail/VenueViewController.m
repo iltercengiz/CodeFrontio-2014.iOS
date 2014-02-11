8be0063e2c59eccfdd9b3c4406d63311f6f15623
Ilter Cengiz <me@iltercengiz.info>
2014-02-11 20:55:23 +0200

    Added VenueViewController

---
+++ Linz/Controller/Detail/VenueViewController.m
@@ -0,0 +1,38 @@
+//
+//  VenueViewController.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 11/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "VenueViewController.h"
+
+@interface VenueViewController ()
+
+@end
+
+@implementation VenueViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
