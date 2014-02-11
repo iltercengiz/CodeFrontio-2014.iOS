0d89f29628212994bccc8bb12b54e357a4618b29
Ilter Cengiz <me@iltercengiz.info>
2014-02-11 20:36:33 +0200

    Commented-out the delegate setting of split view controller lines from AppDelegate

--- Linz/AppDelegate.m
+++ Linz/AppDelegate.m
@@ -13,9 +13,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+/*
     UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
     UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
     splitViewController.delegate = (id)navigationController.topViewController;
+ */
+    
     return YES;
 }
 							
