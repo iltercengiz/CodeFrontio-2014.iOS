95f88662aec5e7e22d0e3603ab54c0170ba3efd7
Ilter Cengiz <me@iltercengiz.info>
2014-02-11 23:44:52 +0200

    Added SessionViewController

---
+++ Linz/Controller/Detail/SessionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SessionViewController.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 11/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SessionViewController : UITableViewController
+
+@end
