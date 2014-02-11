b7d4f6ac10e00f53c2bf6dee9a784f229ff9ce45
Ilter Cengiz <me@iltercengiz.info>
2014-02-11 11:23:01 +0200

    Calendar cells' subclasses are added

---
+++ Linz/View/CalendarSessionCell.m
@@ -0,0 +1,31 @@
+//
+//  CalendarSessionCell.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 11/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "CalendarSessionCell.h"
+
+@implementation CalendarSessionCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Linz/View/CalendarTimeCell.m
@@ -0,0 +1,31 @@
+//
+//  CalendarTimeCell.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 11/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "CalendarTimeCell.h"
+
+@implementation CalendarTimeCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
