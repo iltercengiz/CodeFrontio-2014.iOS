f0e31832bca00d8751ee8b59d93aa0e17167be41
Ilter Cengiz <me@iltercengiz.info>
2014-02-12 22:05:01 +0200

    Added SponsorsViewController

---
+++ Linz/Controller/Detail/SponsorsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SponsorsViewController.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 12/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SponsorsViewController : UITableViewController
+
+@end
