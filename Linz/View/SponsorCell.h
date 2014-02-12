759e39384aec2eee10a9f974605bd7801af67673
Ilter Cengiz <me@iltercengiz.info>
2014-02-12 22:03:40 +0200

    Added SponsorCell

---
+++ Linz/View/SponsorCell.h
@@ -0,0 +1,13 @@
+//
+//  SponsorCell.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 12/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SponsorCell : UITableViewCell
+
+@end
---
+++ Linz/View/SponsorCell.m
@@ -0,0 +1,29 @@
+//
+//  SponsorCell.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 12/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "SponsorCell.h"
+
+@implementation SponsorCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
