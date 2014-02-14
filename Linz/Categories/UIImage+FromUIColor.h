4231284acfad1fa4b2c7c85e537f3f429e433a7f
Ilter Cengiz <ilter@cengiz.im>
2014-02-14 16:12:52 +0200

    A helper UIImage category added
    This category will enable us to create UIImage objects from given UIColor

---
+++ Linz/Categories/UIImage+FromUIColor.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+FromUIColor.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 14/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FromUIColor)
+
+@end
---
+++ Linz/Categories/UIImage+FromUIColor.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+FromUIColor.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 14/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "UIImage+FromUIColor.h"
+
+@implementation UIImage (FromUIColor)
+
+@end
