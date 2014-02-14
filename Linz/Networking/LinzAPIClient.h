73d4cd9c2f62e9be4e9f3cd85424c70cedc5b370
Ilter Cengiz <ilter@cengiz.im>
2014-02-14 11:45:42 +0200

    API client singleton class added

---
+++ Linz/Networking/LinzAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  LinzAPIClient.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 14/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface LinzAPIClient : AFHTTPSessionManager
+
+@end
---
+++ Linz/Networking/LinzAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  LinzAPIClient.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 14/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "LinzAPIClient.h"
+
+@implementation LinzAPIClient
+
+@end
