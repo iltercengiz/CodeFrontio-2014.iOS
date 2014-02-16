efb9b8fd34bc3c584235332f86d0470dd2a1ab61
Ilter Cengiz <me@iltercengiz.info>
2014-02-16 17:12:21 +0200

    Manager singleton class added to manage data

---
+++ Linz/Manager/Manager.h
@@ -0,0 +1,13 @@
+//
+//  Manager.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 16/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Manager : NSObject
+
+@end
---
+++ Linz/Manager/Manager.m
@@ -0,0 +1,13 @@
+//
+//  Manager.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 16/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "Manager.h"
+
+@implementation Manager
+
+@end
