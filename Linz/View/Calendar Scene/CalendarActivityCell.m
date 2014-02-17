7a004b0895e519e2a689b3c1e8fae9d03a255fdc
Ilter Cengiz <ilter@cengiz.im>
2014-02-17 15:54:05 +0200

    CalendarActivityCell is added

---
+++ Linz/View/Calendar Scene/CalendarActivityCell.m
@@ -0,0 +1,31 @@
+//
+//  CalendarActivityCell.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 17/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "CalendarActivityCell.h"
+
+@implementation CalendarActivityCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
