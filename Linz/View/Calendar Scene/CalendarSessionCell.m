cdc9eaf36b8c7e1eb8f5327733d69a64c9019520
Ilter Cengiz <me@iltercengiz.info>
2014-02-18 16:55:40 +0200

    Favourited sessions will fire a notification

--- Linz/View/Calendar Scene/CalendarSessionCell.m
+++ Linz/View/Calendar Scene/CalendarSessionCell.m
@@ -94,10 +94,42 @@ static const CGFloat borderWidth = 0.5;
     
 }
 
+#pragma mark - IBAction
 - (IBAction)favouriteTapped:(id)sender {
     
+    UIButton *favouriteButton = (UIButton *)sender;
+    
+    // De/Select favourite button
+    if (!favouriteButton.selected) {
+        favouriteButton.selected = [self scheduleNotification];
+    } else {
+        favouriteButton.selected = NO;
+        [self cancelNotification];
+    }
+    
+}
+
+#pragma mark - Helpers
+- (BOOL)scheduleNotification {
+    
+    NSTimeInterval timeInterval = [self.session.timeInterval doubleValue] + 15 * 60 + 5; // 15 mins + 5 secs
+    NSDate *sessionTime = [NSDate dateWithTimeIntervalSince1970:timeInterval];
+    
+    // Check if the session is in the future
+    if ([sessionTime compare:[NSDate date]] != NSOrderedDescending) {
+        // Inform user with an alert
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
+                                                        message:NSLocalizedString(@"Date passed error", nil)
+                                                       delegate:nil
+                                              cancelButtonTitle:NSLocalizedString(@"Dismiss", nil)
+                                              otherButtonTitles:nil, nil];
+        [alert show];
+        // Return
+        return NO;
+    }
+    
     // Change state of session's favourite attr.
-    self.session.favourited = @(![self.session.favourited boolValue]);
+    self.session.favourited = @YES;
     
     // Save db
     [[NSManagedObjectContext MR_contextForCurrentThread] MR_saveToPersistentStoreWithCompletion:^(BOOL success, NSError *error) {
@@ -105,8 +137,62 @@ static const CGFloat borderWidth = 0.5;
         else NSLog(@"Save failed with error: %@", error);
     }];
     
-    // De/Select favourite button
-    self.favouriteButton.selected = [self.session.favourited boolValue];
+    // Create a local notification for the session
+    UILocalNotification *notification = ({
+        
+        // Notification will be fired 15 mins before the session
+        // The time interval since 1 Jan 2001 00:00:00
+        NSTimeInterval timeInterval = [self.session.timeInterval doubleValue] - 15 * 60; // 15 mins
+        
+        // Notification
+        UILocalNotification *notification = [UILocalNotification new];
+        // Set the fireDate according to the timeInterval
+        notification.fireDate = [NSDate dateWithTimeIntervalSinceReferenceDate:timeInterval];
+        // String to be shown as 'slide to <string>'
+        notification.alertAction = NSLocalizedString(@"Show", nil);
+        // Notification message
+        notification.alertBody = self.session.title;
+        // Notification's sound
+        notification.soundName = UILocalNotificationDefaultSoundName;
+        // Add the session's identifier to the notification's user info
+        notification.userInfo = @{@"identifier": self.session.identifier};
+        
+        // Return the notification
+        notification;
+        
+    });
+    
+    // Schedule local notification
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    
+    // Return
+    return YES;
+    
+}
+- (void)cancelNotification {
+    
+    // Change state of session's favourite attr.
+    self.session.favourited = @NO;
+    
+    // Save db
+    [[NSManagedObjectContext MR_contextForCurrentThread] MR_saveToPersistentStoreWithCompletion:^(BOOL success, NSError *error) {
+        if (success) NSLog(@"Save successful!");
+        else NSLog(@"Save failed with error: %@", error);
+    }];
+    
+    // Get all scheduled notifications
+    NSArray *notifications = [[UIApplication sharedApplication] scheduledLocalNotifications];
+    
+    // Loop through them to get the notification of the session
+    for (UILocalNotification *notification in notifications) {
+        // If the identifiers are equal, it means that we found our lovely notification <3
+        if ([notification.userInfo[@"identifier"] isEqualToNumber:self.session.identifier]) {
+            // Remove the notification
+            [[UIApplication sharedApplication] cancelLocalNotification:notification];
+            // Break the loop
+            break;
+        }
+    }
     
 }
 
