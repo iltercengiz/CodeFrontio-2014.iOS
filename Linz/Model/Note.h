705bbb578ca76d9119c552168ec1e46d55fc5b66
Ilter Cengiz <me@iltercengiz.info>
2014-02-18 10:20:34 +0200

    Added data model and NSManagedObject subclass for Notes

---
+++ Linz/Model/Note.h
@@ -0,0 +1,18 @@
+//
+//  Note.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 18/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Note : NSManagedObject
+
+@property (nonatomic, retain) NSString * note;
+@property (nonatomic, retain) NSNumber * sessionIdentifier;
+
+@end
