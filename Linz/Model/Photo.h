cb49c12ce96e8a43b1c98957ae1c10a4fbc5f6fb
Ilter Cengiz <ilter@cengiz.im>
2014-02-19 11:59:15 +0200

    Added Photo entity

---
+++ Linz/Model/Photo.h
@@ -0,0 +1,18 @@
+//
+//  Photo.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 19/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Photo : NSManagedObject
+
+@property (nonatomic, retain) NSString * photoURL;
+@property (nonatomic, retain) NSNumber * sessionIdentifier;
+
+@end
---
+++ Linz/Model/Photo.m
@@ -0,0 +1,17 @@
+//
+//  Photo.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 19/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "Photo.h"
+
+
+@implementation Photo
+
+@dynamic photoURL;
+@dynamic sessionIdentifier;
+
+@end
