d7efa47b037868bba10d79da2052def29869ebf4
Ilter Cengiz <ilter@cengiz.im>
2014-02-19 12:07:50 +0200

    Added 'identifier' attribute to the photo model

--- Linz/Model/Photo.h
+++ Linz/Model/Photo.h
@@ -14,5 +14,6 @@
 
 @property (nonatomic, retain) NSString * photoURL;
 @property (nonatomic, retain) NSNumber * sessionIdentifier;
+@property (nonatomic, retain) NSNumber * identifier;
 
 @end
--- Linz/Model/Photo.m
+++ Linz/Model/Photo.m
@@ -13,5 +13,6 @@
 
 @dynamic photoURL;
 @dynamic sessionIdentifier;
+@dynamic identifier;
 
 @end
