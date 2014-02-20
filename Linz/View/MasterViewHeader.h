daab1a558ed82ccf129d68ae564c8aa8dc9aefbb
Ilter Cengiz <me@iltercengiz.info>
2014-02-20 23:29:33 +0200

    MasterViewHeader class added

---
+++ Linz/View/MasterViewHeader.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewHeader.h
+//  Linz
+//
+//  Created by Ilter Cengiz on 20/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewHeader : UIView
+
+@end
