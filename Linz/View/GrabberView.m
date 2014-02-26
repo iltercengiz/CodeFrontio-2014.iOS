e6525e621a999e668543f68ceb03967b8e31a0d9
Ilter Cengiz <ilter@cengiz.im>
2014-02-26 17:55:14 +0200

    Added GrabberView

---
+++ Linz/View/GrabberView.m
@@ -0,0 +1,31 @@
+//
+//  GrabberView.m
+//  Linz
+//
+//  Created by Ilter Cengiz on 26/02/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "GrabberView.h"
+
+@implementation GrabberView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
