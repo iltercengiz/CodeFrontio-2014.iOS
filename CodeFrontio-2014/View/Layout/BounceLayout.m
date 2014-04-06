d11bfec2e9067f4fe2b387f12dac1891ca5b02bc
Ilter Cengiz <me@iltercengiz.info>
2014-04-06 18:25:18 +0300

    Added BounceLayout

---
+++ CodeFrontio-2014/View/Layout/BounceLayout.m
@@ -0,0 +1,13 @@
+//
+//  BounceLayout.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 06/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "BounceLayout.h"
+
+@implementation BounceLayout
+
+@end
