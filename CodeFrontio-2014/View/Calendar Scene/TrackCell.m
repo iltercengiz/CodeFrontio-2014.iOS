18498fcb97d9c936b6cfa71eb4956ac9e54c6668
Ilter Cengiz <me@iltercengiz.info>
2014-04-06 18:43:28 +0300

    Added TrackCell and xib file

---
+++ CodeFrontio-2014/View/Calendar Scene/TrackCell.m
@@ -0,0 +1,31 @@
+//
+//  TrackCell.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 06/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "TrackCell.h"
+
+@implementation TrackCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
