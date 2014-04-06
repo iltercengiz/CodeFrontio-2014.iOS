96ee97498b367b81bca72b588eeec3ccadb33822
Ilter Cengiz <me@iltercengiz.info>
2014-04-06 18:00:21 +0300

    Added PageViewController

---
+++ CodeFrontio-2014/Controller/Base/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 06/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController
+
+@end
