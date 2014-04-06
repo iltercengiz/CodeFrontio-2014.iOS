8fe1d972ebda4cfbb9e7c9e793a279529ee338b0
Ilter Cengiz <me@iltercengiz.info>
2014-04-06 19:07:58 +0300

    Color palette category is added

---
+++ CodeFrontio-2014/Categories/UIColor+Palette.m
@@ -0,0 +1,47 @@
+//
+//  UIColor+Pallete.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 06/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "UIColor+Palette.h"
+
+@implementation UIColor (Palette)
+
++ (UIColor *)P_blueColor {
+    return [UIColor colorWithRed:0.133 green:0.302 blue:0.596 alpha:1];
+}
++ (UIColor *)P_lightBlueColor {
+    return [UIColor colorWithRed:0.071 green:0.498 blue:0.792 alpha:1];
+}
+
++ (UIColor *)P_matteBlueColor {
+    return [UIColor colorWithRed:0.165 green:0.392 blue:0.588 alpha:1];
+}
++ (UIColor *)P_matteLightBlueColor {
+    return [UIColor colorWithRed:0.259 green:0.545 blue:0.788 alpha:1];
+}
+
++ (UIColor *)P_redColor {
+    return [UIColor colorWithRed:0.796 green:0.000 blue:0.051 alpha:1];
+}
++ (UIColor *)P_greenColor {
+    return [UIColor colorWithRed:0.004 green:0.404 blue:0.118 alpha:1];
+}
++ (UIColor *)P_purpleColor {
+    return [UIColor colorWithRed:0.663 green:0.000 blue:0.667 alpha:1];
+}
+
++ (UIColor *)P_whiteColor {
+    return [UIColor colorWithRed:1.000 green:1.000 blue:1.000 alpha:1];
+}
++ (UIColor *)P_blueishWhiteColor {
+    return [UIColor colorWithRed:0.933 green:0.933 blue:0.992 alpha:1];
+}
++ (UIColor *)P_lightGrayColor {
+    return [UIColor colorWithRed:0.937 green:0.937 blue:0.937 alpha:1];
+}
+
+@end
