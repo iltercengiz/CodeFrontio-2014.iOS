bd1c1bfda398cdbc50f7f3ef3b1a1631fed98d97
Ilter Cengiz <ilter@cengiz.im>
2014-04-08 11:43:28 +0300

    Added SessionCell

---
+++ CodeFrontio-2014/View/Calendar Scene/SessionCell.m
@@ -0,0 +1,25 @@
+//
+//  SessionCell.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 08/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "SessionCell.h"
+
+@implementation SessionCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
