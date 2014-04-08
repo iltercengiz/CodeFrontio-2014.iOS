0a457a4ec02c4d9e467492a7d61d219441d25908
Ilter Cengiz <ilter@cengiz.im>
2014-04-08 16:40:20 +0300

    Class and xib files are added for side menu header

---
+++ CodeFrontio-2014/View/SideMenuHeader.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuHeader.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 08/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideMenuHeader : UIView
+
+@end
---
+++ CodeFrontio-2014/View/SideMenuHeader.m
@@ -0,0 +1,31 @@
+//
+//  SideMenuHeader.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 08/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "SideMenuHeader.h"
+
+@implementation SideMenuHeader
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
