51cc5936f80a0b1367d4cdafa110d493ee1fcd89
Ilter Cengiz <ilter@cengiz.im>
2014-04-14 11:22:00 +0300

    Added SpeakerViewController

---
+++ CodeFrontio-2014/Controller/Detail/SpeakerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpeakerViewController.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 14/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpeakerViewController : UIViewController
+
+@end
