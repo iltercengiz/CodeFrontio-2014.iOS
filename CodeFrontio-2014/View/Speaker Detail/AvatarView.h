61551ecd48592a8cae49c5982186b080a14d7fe2
Ilter Cengiz <ilter@cengiz.im>
2014-04-14 11:31:57 +0300

    Additional classes for Speaker scene

---
+++ CodeFrontio-2014/View/Speaker Detail/AvatarView.h
@@ -0,0 +1,13 @@
+//
+//  AvatarView.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 14/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvatarView : UICollectionReusableView
+
+@end
---
+++ CodeFrontio-2014/View/Speaker Detail/AvatarView.m
@@ -0,0 +1,31 @@
+//
+//  AvatarView.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 14/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "AvatarView.h"
+
+@implementation AvatarView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
