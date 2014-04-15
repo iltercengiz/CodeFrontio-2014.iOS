4b2f9466eb7f3784020a9cafcfd9e4586e976581
Ilter Cengiz <ilter@cengiz.im>
2014-04-15 10:25:51 +0300

    Session data model update to hold multiple speakers for a session

--- CodeFrontio-2014/Model/Session.h
+++ CodeFrontio-2014/Model/Session.h
@@ -2,7 +2,7 @@
 //  Session.h
 //  CodeFrontio-2014
 //
-//  Created by Ilter Cengiz on 10/04/14.
+//  Created by Ilter Cengiz on 14/04/14.
 //  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
 //
 
@@ -15,10 +15,9 @@
 @property (nonatomic, retain) NSString * detail;
 @property (nonatomic, retain) NSNumber * favourited;
 @property (nonatomic, retain) NSNumber * identifier;
-@property (nonatomic, retain) NSNumber * speakerIdentifier;
+@property (nonatomic, retain) id speakerIdentifier;
 @property (nonatomic, retain) NSNumber * timeInterval;
 @property (nonatomic, retain) NSString * title;
 @property (nonatomic, retain) NSNumber * track;
-@property (nonatomic, retain) NSNumber * type;
 
 @end
