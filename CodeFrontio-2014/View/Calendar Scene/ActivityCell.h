db9f0de3e230dc4782085e429a7816f861b45bba
Ilter Cengiz <ilter@cengiz.im>
2014-04-15 10:56:03 +0300

    Added ActivityCell with its xib counterpart

---
+++ CodeFrontio-2014/View/Calendar Scene/ActivityCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityCell.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 15/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityCell : UITableViewCell
+
+@end
---
+++ CodeFrontio-2014/View/Calendar Scene/ActivityCell.m
@@ -0,0 +1,25 @@
+//
+//  ActivityCell.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 15/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "ActivityCell.h"
+
+@implementation ActivityCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
