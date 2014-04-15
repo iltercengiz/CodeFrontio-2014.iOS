9efcbe7f2ea8de967a810e3f3ece2049f3081409
Ilter Cengiz <ilter@cengiz.im>
2014-04-15 11:36:39 +0300

    Added SessionCellDouble for sessions with two speakers

---
+++ CodeFrontio-2014/View/Calendar Scene/SessionCellDouble.h
@@ -0,0 +1,13 @@
+//
+//  SessionCellDouble.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 15/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SessionCellDouble : UITableViewCell
+
+@end
---
+++ CodeFrontio-2014/View/Calendar Scene/SessionCellDouble.m
@@ -0,0 +1,25 @@
+//
+//  SessionCellDouble.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 15/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "SessionCellDouble.h"
+
+@implementation SessionCellDouble
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
