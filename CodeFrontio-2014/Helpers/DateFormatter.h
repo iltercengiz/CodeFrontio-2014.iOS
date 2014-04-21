52a034e4e83cee29d013e2ae213536941d592124
Ilter Cengiz <ilter@cengiz.im>
2014-04-21 13:50:10 +0300

    Added DateFormatter helper class

---
+++ CodeFrontio-2014/Helpers/DateFormatter.h
@@ -0,0 +1,13 @@
+//
+//  DateFormatter.h
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 21/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DateFormatter : NSObject
+
+@end
---
+++ CodeFrontio-2014/Helpers/DateFormatter.m
@@ -0,0 +1,13 @@
+//
+//  DateFormatter.m
+//  CodeFrontio-2014
+//
+//  Created by Ilter Cengiz on 21/04/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "DateFormatter.h"
+
+@implementation DateFormatter
+
+@end
